c5079a150dc5e54ea75dd0dfda3df28e87f0501c
CARLOS EDUARDO SILVA PEREIRA <25132872@CFP127ALUNO.LOCAL>
2025-08-05 17:00:00 -0300

    cabeçalho feito.

---
+++ app.js
@@ -0,0 +1,6 @@
+/********************************************************************
+ * Objetivo: Criar uma aplicação para calcuçlar juro composto para um empresa
+ * Autor: Carlos Eduardo
+ * Data: 05/08/2025
+ * Versão 1.0
+ ********************************************************************/
